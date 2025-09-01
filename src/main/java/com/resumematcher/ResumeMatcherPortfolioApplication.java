05da6e2766654250ee983570ffc6a6c8f89fcdbf
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-09-01 15:46:29 -0300

    Spring initialzr

---
+++ src/main/java/com/resumematcher/ResumeMatcherPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.resumematcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeMatcherPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeMatcherPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumematcher/ResumeMatcherPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeMatcherPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
