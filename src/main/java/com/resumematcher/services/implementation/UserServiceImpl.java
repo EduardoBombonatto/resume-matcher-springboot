78027efcb2a1d3049b31b93deca03a59bb879bac
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-09-02 17:11:15 -0300

    feat: Implement all the user service

--- src/main/java/com/resumematcher/services/implementation/UserServiceImpl.java
+++ src/main/java/com/resumematcher/services/implementation/UserServiceImpl.java
@@ -3,13 +3,17 @@ package com.resumematcher.services.implementation;
 import com.resumematcher.dtos.requests.UserRequestDTO;
 import com.resumematcher.dtos.responses.UserResponseDTO;
 import com.resumematcher.exceptions.UserEmailExistsException;
+import com.resumematcher.exceptions.UserNotFoundException;
 import com.resumematcher.models.User;
 import com.resumematcher.repositories.UserRepository;
 import com.resumematcher.services.interfaces.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public abstract class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -26,6 +30,45 @@ public abstract class UserServiceImpl implements UserService {
         }
 
         return this.userEntityToDTO(userRepository.save(user));
+    }
+
+    @Override
+    public UserResponseDTO getUserById(Long id){
+        if(userRepository.existsById(id)){
+            throw new UserNotFoundException();
+        }
+        return this.userEntityToDTO(userRepository.findById(id).get());
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers(){
+        return userRepository.findAll().stream().map(this::userEntityToDTO).collect(Collectors.toList());
+    }
+
+    public UserResponseDTO updateUser(Long id, UserRequestDTO userDto){
+        if(userRepository.existsById(id)){
+            throw new UserNotFoundException();
+        }
+
+        User existingUser = this.userDTOToEntity(userDto);
+        if(existingUser.getName() != null){
+            existingUser.setName(existingUser.getName());
+        }
+
+        if(existingUser.getEmail() != null){
+            existingUser.setEmail(existingUser.getEmail());
+        }
+        return this.userEntityToDTO(userRepository.save(existingUser));
+    }
+
+    @Override
+    public UserResponseDTO deleteUser(Long id){
+        if(userRepository.existsById(id)){
+            throw new UserNotFoundException();
+        }
+        UserResponseDTO userResponseDTO = this.getUserById(id);
+        userRepository.deleteById(id);
+        return userResponseDTO;
     }
 
     private User userDTOToEntity(UserRequestDTO userDto){
